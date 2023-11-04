0de57e5fa05deb64d72eaa7a962e8c271fda2e25
Dherrero870 <dherrero870@gmail.com>
2023-11-04 13:31:25 +0100

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,9 @@
 import math
+import numpy
+import random
+notas = random.sample(range(0,100),25)
+print(notas)
+media = sum(notas)/25
+print (f"La media de las notas es {media}")
+desviacion_estandar = (((sum(notas - media)) ** 2) / 25) ** (1/2) 
+print (f"La desviación estandar es {desviacion_estandar}")
